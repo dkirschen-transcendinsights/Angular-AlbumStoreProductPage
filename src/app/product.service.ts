e3764e39e953b889b72d6032471dbfc11eb4f4a4
David Kirschen <dkirschen@transcendinsights.com>
2019-07-31 15:53:17 -0700

    get products from service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,11 +4,14 @@ import "rxjs/add/operator/map";
 import {Observable} from "rxjs/Observable";
 import {Album} from "./album";
 import {AlbumInfo} from "./album-info";
+import {Product} from "./product";
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -17,5 +20,10 @@ export class ProductService {
     return this._http.get(this._albumUrl).map((response) =>
       <AlbumInfo>response.json());
 
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response =>
+      <Product[]>response.json());
   }
 }
